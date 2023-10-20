ddb53d0cb2b2253677f8223981eec5221bc1c34a
UnityLV <104660372+UnityLV@users.noreply.github.com>
2023-10-20 18:58:07 +0300

    Add FX

--- Assets/Scripts/StickyEffect.cs
+++ Assets/Scripts/StickyEffect.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class StickyEffect : MonoBehaviour
 {
     
     [SerializeField] private GameObject _target;
+
+    private void OnValidate()
+    {
+        if (_target == null)
+        {
+            return;
+        }
+        transform.position = _target.transform.position;
+    }
+
     private void OnEnable()
     {
         transform.parent = null;
